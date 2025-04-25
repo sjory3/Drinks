c82507a381d31fa189c64cba9f3000d93b6421f8
Sjory <Mgj32000@gmail.com>
2025-04-25 10:28:11 +0200

    trying again
    2 sek

--- DrinksMachine/Models/Alcohol.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace DrinksMachine.Models;
-public class Alcohol
-{
-    public string? Name { get; set; }
-    public double Volume { get; set; } // Volume in milliliters
-    public double AlcoholContent { get; set; } // Alcohol content in percentage
-}
--- DrinksMachine/Models/Liquid.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace DrinksMachine.Models;
-
-public class Liquid
-{
-    public string? Name { get; set; }
-    public double Volume { get; set; } // Volume in milliliters
-}
